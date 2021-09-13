eda7f986ebe4c214e467e9781fa9e14aa871de04
Timothy Scheuering <InteractiveTimmy@users.noreply.github.com>
2021-09-13 09:55:44 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/**
+ * An example library for the typescript-template.
+ *
+ * @packageDocumentation
+ */
+
+import {
+  methodA,
+  methodB,
+} from './example-utils';
+
+import Example from './example-class';
+
+import { ExampleInterface } from './example-interface';
+
+export {
+  Example,
+  ExampleInterface,
+  methodA,
+  methodB,
+};
